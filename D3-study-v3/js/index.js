c813575d91c7d91849d5bfe43f9b5f160e268d79
simonxxf <995546883@qq.com>
2019-06-26 12:27:09 +0800

    study

--- D3-study-v3/js/index.js
+++ D3-study-v3/js/index.js
@@ -1,21 +1,52 @@
-var svg = d3.select("#container") 
+ const width = 500
+ const height = 250
+ margin = {left:50,top:30,right:20,bottom:20}
+ g_width = width - margin.left - margin.right
+ g_height = height-margin.top-margin.bottom
+
+
+const svg = d3.select("#container") 
 .append("svg")//生成svg元素
-.attr('width',500)
-.attr("height",250)
+.attr('width',width)
+.attr("height",height)
 
 
 //生成 在svg中g标签
-var g = d3.select('svg')
-.append('g')
-.attr('transform',"translate(50,30)")
+const g = d3.select('svg').append('g')
+
+.attr('transform',`translate(${margin.left},${margin.top})`)
+
+const data =[1,3,5,7,8,4,3,7]
+  
+const scale_x=d3.scaleLinear() //比列尺缩放
+      .domain([0,data.length-1])//输入的范围x坐標軸
+      // .domain(data.map((O,i)=>i))
+      .range([0,g_width])    //输出的范围
+
+const scale_y=d3.scaleLinear()
+      .domain([0,d3.max(data)])//输入的范围y坐標軸
+      .range([g_height,0])
  
-var data =[
-  1,2,3,5,7,8,4,3,7  
-]
 
-var line_generator = d3.line()
-.x((d,i)=>)
 
-d3.select('g')
+const line_generator = d3.line()
+.x((d,i)=>scale_x(i))
+.y((d,i)=>scale_y(d))
+.curve(d3.curveBasis) //曲线拟合的一种方式
+
+g
 .append("path")
-.attr('d',line_generator)
+.attr('d',line_generator(data))
+
+
+const x_axis=d3.axisBottom(scale_x)  //x坐标轴
+
+const y_axis=d3.axisLeft(scale_y)
+
+g.append('g')
+.call(x_axis)
+.attr('transform',`translate(0,${g_height})`)
+
+g.append('g')
+.call(y_axis)
+.attr('transform',`translate(0,0)`)
